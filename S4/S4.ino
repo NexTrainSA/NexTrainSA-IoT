7b9f5e6035c0aa953ea472169352334fd8119128
MariPita <mariana_pita@estudante.sesisenai.org.br>
2025-10-07 08:59:39 -0300

    Implementação de conexão com WiFi

--- S4/S4.ino
+++ S4/S4.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wifi.h>
+WiFiClient cliente;
+const String SSID = "nome da rede";
+const String PASS= "senha da rede";
 
+void setup() {
+  Serial.begin(115200);
+  client.begin(nome,senha);
+  Serial.println("Concetando no Wifi");
+  while(cliente.status()!= WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
 }
 
 void loop() {
