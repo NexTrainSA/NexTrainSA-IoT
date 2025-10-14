051df0327b7b717a0e3d7177f191a396d2eff466
Gabriela166 <gabriela_v_maes@estudante.sesisenai.org.br>
2025-10-14 10:15:33 -0300

    Alterações 14/10

--- S2/S2.ino
+++ S2/S2.ino
@@ -10,11 +10,16 @@ const String PASS = "8120gv08"; //Senha da rede
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
 
+const int pino = 2;
+
+const String topic = "topico_S2";
+
 const String brokerUser = "";
 const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200);
+  pinMode(pino, OUTPUT);
   WiFi.begin(SSID, PASS); //tenta conectar na rede
   Serial.println("Conectando no WiFi");
   while(WiFi.status() != WL_CONNECTED){
@@ -28,12 +33,33 @@ void setup() {
     Serial.println("Conectado ao broker");
     while(mqtt.connect(clientID.c_str()) == 0) {
       Serial.println(".");
-      delay(200);
+      delay(2000);
     }
+    mqtt.subscribe(topic.c_str());
+    mqtt.setCallback(callback);
     Serial.println("\nConectado ao broker!");
   }
   
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  String mensagem = "";
+  if(Serial.available() > 0) {
+    mensagem = Serial.readStringUntil('\n');
+    mensagem = "Gabriela:" + mensagem;
+    Serial.println("A mensagem foi: ");
+    Serial.println (mensagem);
+    mqtt.publish("topicoTrem",mensagem.c_str());
+  }
+  mqtt.loop();
 }
+
+void callback(char* topic, byte* payload, unsigned long length) {
+  String MensagemRecebida = "";
+    for(int i = 0; i < length; i++){
+      //Pega cada letra de payload e junta na mensagem
+      MensagemRecebida += (char) payload[i];
+    }
+    Serial.println(MensagemRecebida);
+    if (MensagemRecebida == "Acender" || "ACENDER") {
+      digitalWrite (pino, HIGH);
+    }
+  }
