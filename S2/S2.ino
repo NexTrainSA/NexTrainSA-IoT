e8318b9d02643e28f5c354500033cf683b156cd2
Gabriela166 <gabriela_v_maes@estudante.sesisenai.org.br>
2025-10-21 10:00:17 -0300

    alteração env.h

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,14 +1,10 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "env.h"
+
 
 WiFiClient wifi_client;
 PubSubClient mqtt(wifi_client);
-
-const String SSID = "FIESC_IOT_EDU"; //nome da rede wifi
-const String PASS = "8120gv08"; //Senha da rede
-
-const String brokerURL = "test.mosquitto.org";
-const int brokerPort = 1883;
 
 const int pino = 2;
 
@@ -20,14 +16,14 @@ const String brokerPass = "";
 void setup() {
   Serial.begin(115200);
   pinMode(pino, OUTPUT);
-  WiFi.begin(SSID, PASS); //tenta conectar na rede
+  WiFi.begin(WIFI_SSID, WIFI_PASS); //tenta conectar na rede
   Serial.println("Conectando no WiFi");
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
     }
     Serial.println("Conectado com sucesso!");
-    mqtt.setServer(brokerURL.c_str(),brokerPort);
+    mqtt.setServer(BROKER_URL.c_str(),BROKER_PORT);
     String clientID = "S2*";
     clientID += String(random(0xffff),HEX);
     Serial.println("Conectado ao broker");
