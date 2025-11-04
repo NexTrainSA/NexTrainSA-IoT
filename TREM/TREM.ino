f96c6773bef9653cf1df6186dd839cf62669d153
Fernanda788 <fernanda_anna@estudante.sesisenai.org.brm>
2025-11-04 09:10:25 -0300

    Feito o código do trem

--- TREM/TREM.ino
+++ TREM/TREM.ino
@@ -6,18 +6,59 @@
 WiFiClientSecure wifiClient;
 PubSubClient mqtt(wifiClient);
 
-const byte dir1 = 4;
+const byte dir1 = 4;  // Mudar depois se necessário
 const byte dir2 = 5;
 
-const byte verdinho = 14;
-const byte vermelhinho = 12;
-const byte azulzinho = 13;
+// Pinos LED RGB:
+const byte vermelhinho = 2;
+const byte verdinho = 4;
+const byte azulzinho = 6;
+
+// Controlando status do LED:
+void statusLED(byte status) {
+  turnOffLEDs();
+  switch (status) {
+    case 254:  // Erro (Vermelho)
+      setLEDColor(255, 0, 0);
+      break;
+    case 1:  // Conectando ao Wi-Fi (Amarelo)
+      setLEDColor(150, 255, 0);
+      break;
+    case 2:  // Conectando ao MQTT (Rosa)
+      setLEDColor(150, 0, 255);
+      break;
+    case 3:  // Movendo para frente (Verde)
+      setLEDColor(0, 255, 0);
+      break;
+    case 4:  // Movendo para trás (Ciano)
+      setLEDColor(0, 255, 255);
+      break;
+    default:
+      for (byte i = 0; i < 4; i++) {
+        setLEDColor(0, 0, 255);  // Erro no status (pisca azul)
+        delay(100);
+        turnOffLEDs();
+        delay(100);
+      }
+      break;
+  }
+}
+
+void turnOffLEDs() {
+  setLEDColor(0, 0, 0);
+}
+
+void setLEDColor(byte r, byte g, byte b) {
+  ledcWrite(vermelhinho, r);
+  ledcWrite(verdinho, g);
+  ledcWrite(azulzinho, b);
+}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Mensagem recebida em ");
   Serial.println(topic);
 
-  // Acho que primeiro converte de byte para char... 
+  // Acho que primeiro converte de byte para char...
   String msg = "";
   for (int i = 0; i < length; i++) {
     msg += (char)payload[i];
@@ -27,25 +68,55 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Velocidade recebida: ");
   Serial.println(valor);
 
-  if (String(topic) == "NexTrain/TREM/Atuadores/SPEED") {
-    if (valor > 0) {
+  if (String(topic) == TOPIC_SPEED) {
+    if (valor > 0) {  // Move para frente
       digitalWrite(dir1, HIGH);
       digitalWrite(dir2, LOW);
-      digitalWrite(verdinho, HIGH);
-      digitalWrite(vermelhinho, LOW);
-      digitalWrite(azulzinho, LOW);
-    } else if (valor < 0) {
+      statusLED(3);
+    } else if (valor < 0) {  // Ré
       digitalWrite(dir1, LOW);
       digitalWrite(dir2, HIGH);
-      digitalWrite(azulzinho, HIGH);
-      digitalWrite(verdinho, LOW);
-      digitalWrite(vermelhinho, LOW);
-    } else {
+      statusLED(4);
+    } else {  // Parado
       digitalWrite(dir1, LOW);
       digitalWrite(dir2, LOW);
-      digitalWrite(vermelhinho, HIGH);
-      digitalWrite(verdinho, LOW);
-      digitalWrite(azulzinho, LOW);
+      statusLED(254)
+    }
+  }
+}
+
+void connectToWifi {
+  // Conectando à Internet:
+  statusLED(2);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("Conectando à Internet...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Falha ao conectar.");
+    statusLED(254);
+    delay(200);
+  }
+  Serial.println("\nConectado à Internet!");
+}
+
+void connectToMQTT() {
+  // Comunicação com broker:
+  statusLED(2);
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  mqtt.setCallback(callback);
+
+  String clientID = "TREM-";
+  clientID += String(random(0xffff), HEX);
+  Serial.println("Conectando ao Broker...");
+
+    while (!mqtt.connected()) {
+    if (mqtt.connect(clientID.c_str())) {
+      Serial.println("\nConectado ao Broker!");
+      mqtt.subscribe(TOPIC_SPEED);
+    } else {
+      Serial.print("Falha ao conectar.");
+      statusLED(254);
+      delay(2000);
     }
   }
 }
@@ -54,47 +125,13 @@ void setup() {
   Serial.begin(115200);
   wifiClient.setInsecure();
 
-  pinMode(verdinho, OUTPUT);
-  pinMode(vermelhinho, OUTPUT);
-  pinMode(azulzinho, OUTPUT);
   pinMode(dir1, OUTPUT);
   pinMode(dir2, OUTPUT);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.println("Conectando à Internet...");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(200);
-  }
-
-  Serial.println("\nConectado à Internet!");
-
-  // Comunicação com broker:
-  mqtt.setServer(BROKER_URL, BROKER_PORT);
-  mqtt.setCallback(callback);
-
-  String clientID = "TREM-";
-  clientID += String(random(0xffff), HEX);
-  Serial.println("Conectando ao Broker...");
-
-  while (!mqtt.connected()) {
-    if (mqtt.connect(clientID.c_str())) {
-      Serial.println("\nConectado ao Broker!");
-      mqtt.subscribe(TOPIC_SPEED);
-    } else {
-      Serial.print(".");
-      delay(2000);
-    }
-  }
+  ledcAttach(vermelhinho, 5000, 8);
+  ledcAttach(verdinho, 5000, 8);
+  ledcAttach(azulzinho, 5000, 8);
 }
 
 void loop() {
-  // Publicar:
-  if (Serial.available() > 0) {
-    String mensagem = Serial.readStringUntil('\n');
-    mqtt.publish(TOPIC_RGB_4, mensagem.c_str());
-  }
-
   mqtt.loop();
 }
